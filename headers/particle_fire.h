60e65b706bbe9c4e8db9778bfbc72a951f694be8
riku zukale <rzukale@student.hive.fi>
2021-10-16 16:22:10 +0300

    Added a few header inclusions, Tested initting SDL and quitting

--- headers/particle_fire.h
+++ headers/particle_fire.h
@@ -6,7 +6,7 @@
 /*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 14:52:27 by rzukale           #+#    #+#             */
-/*   Updated: 2021/10/16 14:53:01 by rzukale          ###   ########.fr       */
+/*   Updated: 2021/10/16 16:15:59 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #define PARTICLE_FIRE_H_
 
 #include <iostream>
+#include <cstdlib>
+#include "../SDL2/include/SDL2/SDL.h"
 using namespace std;
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,12 +6,17 @@
 /*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 14:53:05 by rzukale           #+#    #+#             */
-/*   Updated: 2021/10/16 14:53:35 by rzukale          ###   ########.fr       */
+/*   Updated: 2021/10/16 16:15:03 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/particle_fire.h"
 
 int main() {
-	return 0;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL Init failed" << endl;
+		return EXIT_FAILURE;
+	}
+	SDL_Quit();
+	return EXIT_SUCCESS;
 }
